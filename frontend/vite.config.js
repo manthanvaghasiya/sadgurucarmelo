f2f6792c77d92fdac217978bab11d0bec77a792f
ManthanVaghasiya <manthanvaghasiya60@gmail.com>
2026-04-22 17:30:29 +0530

    pwa fixed icon validation

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -22,17 +22,17 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: '/logo1.png',
+            src: '/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/logo1.png',
+            src: '/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: '/logo1.png',
+            src: '/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
